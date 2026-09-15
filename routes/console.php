ddaba7b5767e5fab7f0cd86e38d6213a1323eec9
Erik Gabriel <erikgab10@gmail.com>
2026-09-15 13:22:02 -0400

    feat: agenda verificação de manutenção"

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Foundation\Inspiring;
+use App\Jobs\VerificarManutencaoJob;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::job(new VerificarManutencaoJob)->dailyAt('02:00');
